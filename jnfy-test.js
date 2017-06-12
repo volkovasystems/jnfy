ee76360eb33149a534a49baa3c682887dc3935e2
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-12 12:33:31 +0800

    Updated server/client test.

--- jnfy-test.js
+++ jnfy-test.js
@@ -2,6 +2,7 @@
 const assert = require( "assert" );
 const jnfy = require( "./jnfy.js" );
 
+assert.equal( jnfy( { "name": "simple" } ), '{"name":"simple"}', "should be deeply equal" );
 assert.ok( jnfy( global ) );
 
 console.log( "ok" );
--- jnfy.support.js
+++ jnfy.support.js
@@ -35,6 +35,9 @@
               			"module": "jnfy",
               			"author": "Richeve S. Bebedor",
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/jnfy.git",
               			"test": "jnfy-test.js",
               			"global": true
